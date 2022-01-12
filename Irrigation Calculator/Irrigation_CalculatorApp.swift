ec662cdeb8f5fbe4c593d73d13b511359fa8fd04
hansen-ri <gridironhero@gmail.com>
2022-01-12 11:22:30 -0700

    Initial Commit

---
+++ Irrigation Calculator/Irrigation_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Irrigation_CalculatorApp.swift
+//  Irrigation Calculator
+//
+//  Created by Riley Hansen on 1/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Irrigation_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
