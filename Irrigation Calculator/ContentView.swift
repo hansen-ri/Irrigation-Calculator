e7706a6ff6cb5d0324e6f3397a2868e6bb9a8111
hansen-ri <gridironhero@gmail.com>
2022-01-13 22:29:46 -0700

    Buttons and menus layout 1.0

--- Irrigation Calculator/ContentView.swift
+++ Irrigation Calculator/ContentView.swift
@@ -18,31 +18,113 @@ struct ContentView: View {
             
             VStack {
                 
+                
+                let buttonLabel = Text("CALCULATE")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(hue: 0.196, saturation: 1.0, brightness: 1.0))
+                
+                let buttonLabel2 = Text("Enter")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.white)
+                
                 Spacer()
-                Text("Irrigation Calculator").font(.largeTitle).fontWeight(.semibold).foregroundColor(Color(hue: 0.618, saturation: 0.908, brightness: 0.697)).dynamicTypeSize(/*@START_MENU_TOKEN@*/.xxxLarge/*@END_MENU_TOKEN@*/).border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
+                Text("Irrigation Calculator").font(.largeTitle).fontWeight(.semibold).foregroundColor(Color(hue: 0.618, saturation: 0.908, brightness: 0.697)).dynamicTypeSize(/*@START_MENU_TOKEN@*/.xxxLarge/*@END_MENU_TOKEN@*/)
             
                 
                 HStack {
                 
                 VStack {
-                    Text("Hstack")
-                    Text("more hstack")
-                }
-                .frame(width: 200.0, height: 725.0)
+
+                    
+                    TextField("Water 1", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                    TextField(/*@START_MENU_TOKEN@*/"Water 2"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    TextField("Water 3", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    TextField(/*@START_MENU_TOKEN@*/"Water 4"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    TextField(/*@START_MENU_TOKEN@*/"Water 5"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                    TextField("Placeholder", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180.0, height: 40.0)
+                        .border(Color.black, width: 3)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 1.0, saturation: 0.022, brightness: 0.373, opacity: 0.573)/*@END_MENU_TOKEN@*/)
+                    
+                                    }
+                .padding()
+                .frame(width: 200.0, height: 600.0)
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
                 
                     
                 VStack {
-                    Text("Hstack")
-                    Text("more hstack")
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
+                    Button("\(buttonLabel2)"){}
+                    .frame(width: 150.0, height: 40.0)
+                    .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    
                 }
-                .frame(width: 200.0, height: 725.0)
+                .frame(width: 200.0, height: 600.0)
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
                 
                 }
-                .frame(width: 425.0, height: 750.0)
-            .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/4/*@END_MENU_TOKEN@*/)
+                .frame(width: 425.0, height: 650.0)
+            
+            
+                Button("\(buttonLabel)"){}
+                .frame(width: 225.0, height: 75.0)
+                .background(Color(hue: 0.618, saturation: 0.908, brightness: 0.697))
+                .cornerRadius(/*@START_MENU_TOKEN@*/40.0/*@END_MENU_TOKEN@*/)
+                
+                Spacer()
             }
+            
             .frame(height: 875.0)
         }
     }
